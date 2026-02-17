94e9384feeb3242580c9f5d9bbc0419d9eec506a
Ayyan Nadeem <ayyan137a@gmail.com>
2026-02-17 23:36:37 +0500

    NavLink.tsx

--- src/components/NavLink.tsx
+++ src/components/NavLink.tsx
@@ -1,7 +1,7 @@
 import { NavLink as RouterNavLink, NavLinkProps } from "react-router-dom";
 import { forwardRef } from "react";
 import { cn } from "@/lib/utils";
-
+//nav link
 interface NavLinkCompatProps extends Omit<NavLinkProps, "className"> {
   className?: string;
   activeClassName?: string;
